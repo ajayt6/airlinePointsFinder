eb0ec947cbb540f8d62b9e96c87dca64bb7535a2
Ajay Thomas <ajayt6@yahoo.com>
2024-04-11 19:12:44 -0700

    Basic cleanup

--- airlinesScraper.py
+++ airlinesScraper.py
@@ -1,12 +1,10 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
 import time
 from datetime import datetime
+
+from bs4 import BeautifulSoup
 from dateutil.rrule import rrule, DAILY
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 def get_date_input(prompt):
@@ -31,10 +29,6 @@ def main():
     urls = [(f"https://www.point.me/results?departureCity=Seattle&departureIata=SEA&arrivalCity=Charlotte&arrivalIata"
              f"=CLT&legType=oneWay&classOfService=economy&passengers=1&pid=&depar"
              f"tureDate={date.strftime('%Y-%m-%d')}&arrivalDate=2024-07-29") for date in dates]
-
-    # search_url = (("https://www.point.me/results?departureCity=Seattle&departureIata=SEA&arrivalCity=Charlotte"
-    #               "&arrivalIata=CLT&legType=oneWay&classOfService=economy&passengers=1&pid=&departureDate=") +
-    #               departure_date + "&arrivalDate=2024-07-29")
 
     # Initialize the driver
     driver = webdriver.Chrome()
@@ -75,10 +69,6 @@ def main():
         else:
             driver.execute_script(f"window.open('{url}');")
         time.sleep(1)  # Delay between opening each tab
-    # driver.get(search_url)
-
-    #search_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Search')]")
-    #search_button.click()
 
     time.sleep(page_load_wait_time)
 
@@ -90,20 +80,6 @@ def main():
         driver.switch_to.window(driver.window_handles[i])
         soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-        print(soup.prettify())
-
-        # Switch to tab
-        # driver.switch_to.window(driver.window_handles[0])
-
-        # Assuming `driver.page_source` contains the HTML source
-        # soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-        # print(soup.prettify())
-        with open('temp.txt', 'w') as f:
-            f.write(f"{soup}\n")
-
-        results = []
-
         # Find all divs that have a class attribute starting with 'result_'
         results = soup.find_all(
             lambda tag: tag.name == "div" and tag.get("class", "") and tag["class"][0].startswith("result_"))
@@ -111,7 +87,6 @@ def main():
         with open('results.txt', 'w') as f:
             f.write(f"{results}\n")
 
-        date_airlines = {}
         airlines = []
 
         with open('airlines.txt', 'a') as f:
@@ -124,10 +99,6 @@ def main():
             if economy_div:
                 # Navigating up to b
                 b_div = economy_div.find_parent().find_parent().find_parent()
-
-                print("\n")
-                print(b_div)
-                print("\n")
 
                 # Find the sibling div of b and then search for the nested child containing "pts"
                 c_div = b_div.find_next_sibling("div")
@@ -146,7 +117,6 @@ def main():
                 airline_div = economy_div.find_previous_sibling("div")
                 if airline_div:
                     airline = airline_div.get_text(strip=True)
-                    # airlines.append(airline_div.text.strip())
                     airlines.append(f"{airline}: {points}")
 
         # Saving the airline names into a file
